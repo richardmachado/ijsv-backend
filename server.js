2cf87872ef4adf6a38f327019bfff74711da029a
Richard Machado <mmachado.richard@gmail.com>
2021-03-01 20:12:09 -0800

    add cors and helmet

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
-
+const cors = require("cors");
+const helmet = require("helmet");
 const server = express();
 
 const loginRouter = require("./auth/auth-router");
 const temasRouter = require("./temas/temas-router");
 
+server.use(helmet());
+server.use(cors());
 server.use(express.json());
 
 server.use("/api/auth", loginRouter);
