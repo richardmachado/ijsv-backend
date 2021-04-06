8242f78553c364b46bba611402cd8b21372bf52f
Richard Machado <mmachado.richard@gmail.com>
2021-04-06 15:14:06 -0700

    correct production connection with ssl=no verify

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: `${process.env.DATABASE_URL}?ssl=no-verify`,
     pool: {
       min: 2,
       max: 10,
