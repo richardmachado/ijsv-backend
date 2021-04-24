18564d4a8414349ff86d4d3b74acf0430ee56b35
Richard Machado <mmachado.richard@gmail.com>
2021-04-24 12:35:50 -0700

    checks if pushing an update will reset seeds

--- data/db-config.js
+++ data/db-config.js
@@ -1,7 +1,7 @@
 const knex = require("knex");
 const knexfile = require("../knexfile.js");
 
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 const config = knexfile[env];
-
+console.log("you are currently working on ", env, "server");
 module.exports = knex(config);
