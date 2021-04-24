8bc83508da72b311414347ad4bc0cd6678f55cf6
Richard Machado <mmachado.richard@gmail.com>
2021-04-24 08:43:14 -0700

    fixes db-config.js

--- knexfile.js
+++ knexfile.js
@@ -26,10 +26,6 @@ module.exports = {
   production: {
     client: "pg",
     connection: `${process.env.DATABASE_URL}?ssl=no-verify`,
-    pool: {
-      min: 2,
-      max: 10,
-    },
     migrations: {
       directory: "./migrations",
     },
